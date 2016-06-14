f9478575326b1b1c03adb71ef7f8538bbb25680b
kriangkrai ch <itemcomputer.gp@gmail.com>
2016-06-14 19:40:49 +0700

    new 62 ทุ่ม

---
+++ application/views/todolist/L001.php
@@ -0,0 +1,75 @@
+<?php echo $tempheader; ?>			
+	    <div class="jumbotronaon">
+				 <div class="page-header">
+				   	<center><h2><font color="#2a3f52">TODO LIST</font></h2> </center>    
+				 </div>
+					  <p>คุณต้องทำอะไรบ้างวันนี้...</p> 
+			<center>
+			<div class="row myBox" >
+		    	<div class="col-md-12">
+		    		<div class="content-table">
+		    		<table class="box-table">
+					    <thead>
+					      <tr>
+					        <th class="fonthead-table">วัน/เดือน/ปี</th>
+					        <th class="fonthead-table">หัวข้อ</th>
+					        <th class="fonthead-table">โครงการ</th>
+					        <th class="fonthead-table">รายละเอียด</th>
+					        <th class="fonthead-table">ระดับความสำคัญ</th>
+					        <th class="fonthead-table">สถานะ</th>
+					        <th class="fonthead-table">ผู้มอบหมายงาน</th>
+					        <th class="fonthead-table">เสร็จสิ้นในวันที่</th>
+					        <th class="fonthead-table">เริ่ม</th>
+					        <th class="fonthead-table">เสร็จสิ้น</th>
+					      </tr>
+					    </thead>
+
+					    <tbody>
+					       <tr>
+					    		<td><input type="date" class="form-control" id="sm " value=""></td>
+						        <td><input type="text" class="form-control"  id="sm" style="width:100px;"></td>
+						        <td><select class="form-control" id="sel1" style="width:100px;" >
+								        <option>1</option>
+								        <option>2</option>
+								        <option>3</option>
+								        <option>4</option>
+								      </select>
+								</td>
+						        <td><input class="form-control" id="focusedInput" type="text" value="รายละเอียด" style="width:100px;"></td>
+						        <td><select class="form-control" id="sel1" style="width:100px;" >
+								        <option>เลือก</option>
+								        <option>เลือก</option>
+								        <option>เลือก</option>
+								        <option>เลือก</option>
+								      </select>
+								</td>
+						       <td><select class="form-control" id="sel1" style="width:100px;" >
+								        <option>เลือก</option>
+								        <option>เลือก</option>
+								        <option>เลือก</option>
+								        <option>เลือก</option>
+								      </select>
+								</td>
+						        <td><select class="form-control" id="sel1" style="width:100px;" >
+								        <option>เลือก</option>
+								        <option>เลือก</option>
+								       
+								      </select>
+								</td>
+						        <td><input type="text" class="form-control" id="sm " style="width:70px;"></td>
+						         <td><input type="text" class="form-control" id="sm " style="width:70px;"></td>
+						        <td><input type="text" class="form-control" id="sm " style="width:70px;"></td>
+					      	 </tr>
+					    </tbody>
+					 </table>
+					 </div>
+		    	</div>
+		    </div> 
+		    </center> 
+	    </div>
+</body>
+</html>   
+<?php echo $tempfooter; ?>				
+
+
+
